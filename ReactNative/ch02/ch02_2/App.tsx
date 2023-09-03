e7483dc687d6a7c9e11ccf3ceb9683764b7003d4
hwanheejung <hwanheejung117@gmail.com>
2023-09-04 04:01:35 +0900

    ReactNative ch02_2 array1

--- ReactNative/ch02/ch02_2/App.tsx
+++ ReactNative/ch02/ch02_2/App.tsx
@@ -2,25 +2,12 @@ import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
 export default function App(){
-  const isLoading = true
-  // if(isLoading){
-  //   return (
-  //     <SafeAreaView>
-  //       <Text>Loading...</Text>
-  //     </SafeAreaView>
-  //   )
-  // }
-  // return (
-  //   <SafeAreaView>
-  //     {isLoading && <Text>Loading...</Text>}
-  //     {isLoading && <Text>Hello JSX world~!</Text>}
-  //     </SafeAreaView>
-  // )
-  const children = isLoading ? (
-    <Text>Loading...</Text>
-  ): (
-    <Text>Hello JSX world~!</Text>
-  )
+  // const children = [
+  //   <Text>Hello</Text>,
+  //   <Text>Hello</Text>,
+  //   <Text>Hello</Text>
+  // ]
+  const children = [1, 2, 3].map((i) => <Text>Hello {i}</Text>)
   return <SafeAreaView>{children}</SafeAreaView>
   
 }
