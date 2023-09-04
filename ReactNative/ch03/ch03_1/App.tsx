554f80223ecc38da51d026d6fbb6b7e5d7ef6e18
hwanheejung <hwanheejung117@gmail.com>
2023-09-04 23:17:20 +0900

    ch03_1 MD2Colors

--- ReactNative/ch03/ch03_1/App.tsx
+++ ReactNative/ch03/ch03_1/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import {MD2Colors} from 'react-native-paper'
 
+console.log(MD2Colors.blue500)
 export default function App(){
   return (
-    <SafeAreaView style={[styles.safeAreaView, {backgroundColor: 'blue'}]}>
-      <Text style={[styles.text, {color: 'white'}]}>Hello StyleSheet World!</Text>
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Text style={[styles.text]}>Hello StyleSheet World!</Text>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
-  text: {fontSize: 20}
+  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: MD2Colors.blue500},
+  text: {fontSize: 20, color: MD2Colors.blue200}
 })
