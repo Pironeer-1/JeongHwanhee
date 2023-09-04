74e8622ed0b3d72e7176d45fc94001dbbd8c87f2
hwanheejung <hwanheejung117@gmail.com>
2023-09-04 23:00:47 +0900

    ch03_1 stylesheet API

---
+++ ReactNative/ch03/ch03_1/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text } from 'react-native';
+
+export default function App(){
+  return (
+    <SafeAreaView style={[styles.safeAreaView, {backgroundColor: 'blue'}]}>
+      <Text style={[styles.text, {color: 'white'}]}>Hello StyleSheet World!</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  text: {fontSize: 20}
+})
