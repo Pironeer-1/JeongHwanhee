1f43817c90901702b5edbfcc8000e6f7eeb19903
hwanheejung <hwanheejung117@gmail.com>
2023-09-05 00:33:47 +0900

    ch03_2

--- ReactNative/ch03/ch03_2/App.tsx
+++ ReactNative/ch03/ch03_2/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {StyleSheet, Dimensions, SafeAreaView, Text, Platform} from 'react-native'
-import {MD2Colors} from 'react-native-paper'
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, Dimensions, SafeAreaView, Text, Platform, View} from 'react-native'
+import {MD2Colors} from 'react-native-paper' 
 
 const {width, height} = Dimensions.get('window')
 
@@ -11,10 +10,16 @@ export default function App(){
       <Text style={[styles.text]}>os: {Platform.OS}</Text>
       <Text style={[styles.text]}>width: {width} px</Text>
       <Text style={[styles.text]}>height: {height} px</Text>
+
+      <View style={[styles.box, styles.border]}></View>
+      <View style={[styles.box, styles.border, {borderRadius: 20}]}></View>
+      <View style={[styles.box, styles.border, {borderTopLeftRadius: 40, borderBottomLeftRadius: 40}]}></View>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
-  safeAreaView: {backgroundColor: MD2Colors.blue500, height},
-  text: {fontSize: 20, color: MD2Colors.blue200}
+  safeAreaView: {backgroundColor: MD2Colors.blue500, flex: 1, paddingLeft: Platform.select({ios: 0, android: 20})},
+  text: {fontSize: 20, color: MD2Colors.blue200},
+  box: {height: 100, backgroundColor: MD2Colors.lime500, marginBottom: 10, marginLeft: Platform.select({ios: 20, android: 0})},
+  border: {borderWidth: 10, borderColor: MD2Colors.blue900},
 })
