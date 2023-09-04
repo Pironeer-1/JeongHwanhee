1aa2665616591fa7593825381a969a828175f772
hwanheejung <hwanheejung117@gmail.com>
2023-09-04 15:34:33 +0900

    ch02_4 touchable

--- ReactNative/ch02/ch02_4/App.tsx
+++ ReactNative/ch02/ch02_4/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { SafeAreaView, Alert, Button } from "react-native";
+import { TouchableHighlight, TouchableOpacity, Text } from "react-native";
+
+const onPress = () => {
+  Alert.alert('home pressed.', 'message')
+}
 
 export default function App(){
   return (
   <SafeAreaView>
-    <Button title='Home'
-            onPress={() => {
-              Alert.alert('home pressed', 'message')
-            }} />
+    <Button title='Home' onPress={onPress} />
+    <TouchableOpacity onPress={onPress}>
+      <Text>TouchableOpacity</Text>
+    </TouchableOpacity>
+    <TouchableHighlight onPress={onPress}>
+      <Text>TouchableHighlight</Text>
+    </TouchableHighlight>
   </SafeAreaView>)
 }
