b9bfd81b468e68d2bb09708d24e169726ecbce61
hwanheejung <hwanheejung117@gmail.com>
2023-09-04 14:40:42 +0900

    ReactNative ch02_3 scrollview

--- ReactNative/ch02/ch02_3/App.tsx
+++ ReactNative/ch02/ch02_3/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, ScrollView, Text } from 'react-native'
 import ClassComponent from './src/screens/ClassComponent'
 import ArrowComponent from './src/screens/ArrowComponent'
 import Person from './src/screens/Person'
 import * as D from './src/data'
 
 const person = D.CreateRandomPerson()
+const people = D.makeArray(100).map(D.CreateRandomPerson)
 
 export default function App() {
+  const children = people.map((person) => {
+    <Person key={person.id} person={person}></Person>
+  })
   return (
     <SafeAreaView>
-      <ClassComponent />
-      <ArrowComponent />
-      <Person person={person} />
+      <ScrollView>{children}</ScrollView>
     </SafeAreaView>
   )
 }
