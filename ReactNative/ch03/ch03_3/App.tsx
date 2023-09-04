116abd484450f6937b9c625047d8a67a9408fe50
hwanheejung <hwanheejung117@gmail.com>
2023-09-05 01:04:18 +0900

    ch03_3 ImageBackground

---
+++ ReactNative/ch03/ch03_3/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, SafeAreaView, ImageBackground} from "react-native";
+
+
+export default function App(){
+  return (
+  <SafeAreaView style={styles.flex}>
+    <ImageBackground 
+      style={[styles.flex]}
+      source={require('./src/assets/images/bg.jpg')} />
+  </SafeAreaView>)
+}
+
+const styles = StyleSheet.create({
+  flex: {flex: 1}
+})
