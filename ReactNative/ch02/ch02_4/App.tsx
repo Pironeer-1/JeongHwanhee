d7ed5822b51fa6df4e33dedd2ae8ecba5dc3c327
hwanheejung <hwanheejung117@gmail.com>
2023-09-04 15:31:30 +0900

    ch02_4 button

---
+++ ReactNative/ch02/ch02_4/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Alert, Button } from "react-native";
+
+export default function App(){
+  return (
+  <SafeAreaView>
+    <Button title='Home'
+            onPress={() => {
+              Alert.alert('home pressed', 'message')
+            }} />
+  </SafeAreaView>)
+}
