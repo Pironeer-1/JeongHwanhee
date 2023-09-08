3687d3eb1d5d6f37defd8794f586b232fa510327
hwanheejung <hwanheejung117@gmail.com>
2023-09-08 17:43:13 +0900

    20, 21, 22

---
+++ web2-nodejs/syntax/array.js
@@ -0,0 +1,4 @@
+var arr = ['A', 'B', 'C'];
+arr.push('D');
+console.log(arr);
+console.log(arr.length);
---
+++ web2-nodejs/syntax/loop.js
@@ -0,0 +1,9 @@
+console.log('A');
+console.log('B');
+var i = 0;
+while(i < 2) {
+    console.log('chicken');
+    console.log('babo');
+    i = i + 1;
+}
+console.log('D')
