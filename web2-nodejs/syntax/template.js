8e0a4f322757970364637b4c7388440120f656e0
hwanheejung <hwanheejung117@gmail.com>
2023-09-08 16:21:26 +0900

    08. 템플릿 리터럴

---
+++ web2-nodejs/syntax/template.js
@@ -0,0 +1,8 @@
+var name2 = 'egoing';
+var letter = `Dear ${name2}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+${name2} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in ${name2} culpa qui officia deserunt mollit anim id est laborum. ${name2}`;
+console.log(letter);
