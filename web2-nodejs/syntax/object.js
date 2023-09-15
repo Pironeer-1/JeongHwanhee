8cb04e8e015cb100ee1f84710fde30e7e786ae86
hwanheejung <hwanheejung117@gmail.com>
2023-09-15 20:31:06 +0900

    #42: 객체(데이터와 값을 담는 그릇으로서의 함수)

--- web2-nodejs/syntax/object.js
+++ web2-nodejs/syntax/object.js
@@ -21,15 +21,29 @@ while(i < members.length) {
 //   console.log("object => ", n, "value => ", roles[n]);
 // }
 
-var f = function () {
-  console.log(1 + 1);
-  console.log(1 + 2);
-};
+// var f = function () {
+//   console.log(1 + 1);
+//   console.log(1 + 2);
+// };
 
-var a = [f];
-a[0]();
+// var a = [f];
+// a[0]();
+
+// var o = {
+//   func: f,
+// };
+// o.func();
 
 var o = {
-  func: f,
+  v1: "v1",
+  v2: "v2",
+  f1: function () {
+    console.log(this.v1);
+  },
+  f2: function () {
+    console.log(this.v2);
+  },
 };
-o.func();
+
+o.f1();
+o.f2();
